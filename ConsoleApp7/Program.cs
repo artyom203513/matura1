486f47409d5ce4e2652e7cf481a356f5afd90230
negr <martinpolakmp@seznam.cz>
2024-09-08 16:40:24 +0200

    hovna

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("prvni cislo");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("druhe cislo");
+int num2 = int.Parse(Console.ReadLine());
+
+int vysledek = num1 + num2;
+
+Console.WriteLine(vysledek);
